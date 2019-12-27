85dee70e8e9cd5034b9e824b055acb00858d16f8
kuangklq <kuangklq@126.com>
2019-12-27 15:43:26 +0800

    python_basic

---
+++ averagen.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+N = 10
+sum = 0
+count = 0
+print("pls input 10 numbers:")
+while count < N:
+    number = float(input())
+    sum += number
+    count += 1
+average = sum / N
+print("N = {}, Sum = {}".format(N, sum))
+print("Average = {:.2f}".format(average))
+
